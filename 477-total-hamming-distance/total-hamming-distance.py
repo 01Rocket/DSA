968b1aeab6b1b4ef99debb3e3f443cad3a49e3e2
Nehal Verma <142144242+01Rocket@users.noreply.github.com>
2026-06-27 10:10:30 +0530

    Time: 160 ms (60.09%) | Memory: 20.6 MB (87.79%) - LeetSync

---
+++ 477-total-hamming-distance/total-hamming-distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalHammingDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = 0
+
+        for bit in range(32):
+            ones = 0
+
+            for num in nums:
+                if (num >> bit) & 1:
+                    ones += 1
+
+            zeros = n - ones
+            total += ones * zeros
+
+        return total
