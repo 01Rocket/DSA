958be13373b26366e6943ecfc9172cb68e9d25fa
Nehal Verma <142144242+01Rocket@users.noreply.github.com>
2026-06-29 12:47:46 +0530

    Time: 15 ms (82.49%) | Memory: 21.1 MB (55.65%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.py
@@ -0,0 +1,6 @@
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        unique_types = len(set(candyType))
+        allowed = len(candyType) // 2
+
+        return min(unique_types, allowed)
