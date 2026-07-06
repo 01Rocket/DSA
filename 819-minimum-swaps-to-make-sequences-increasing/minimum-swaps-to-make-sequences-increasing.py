ce7bdde3033c5dea7db1df5b23160799d5acc00f
Nehal Verma <142144242+01Rocket@users.noreply.github.com>
2026-07-06 17:33:27 +0530

    Time: 139 ms (44.20%) | Memory: 36.9 MB (45.65%) - LeetSync

---
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+
+        keep = 0
+        swap = 1
+
+        for i in range(1, n):
+            new_keep = float("inf")
+            new_swap = float("inf")
+
+            # No swap needed
+            if nums1[i] > nums1[i - 1] and nums2[i] > nums2[i - 1]:
+                new_keep = min(new_keep, keep)
+                new_swap = min(new_swap, swap + 1)
+
+            # Swap at current or previous position
+            if nums1[i] > nums2[i - 1] and nums2[i] > nums1[i - 1]:
+                new_keep = min(new_keep, swap)
+                new_swap = min(new_swap, keep + 1)
+
+            keep = new_keep
+            swap = new_swap
+
+        return min(keep, swap)
