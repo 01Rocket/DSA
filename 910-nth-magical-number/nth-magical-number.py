03f7d74880a034a19947b199540e2588a52b1d0e
Nehal Verma <142144242+01Rocket@users.noreply.github.com>
2026-07-11 10:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.54%) - LeetSync

---
+++ 910-nth-magical-number/nth-magical-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def nthMagicalNumber(self, n: int, a: int, b: int) -> int:
+        MOD = 10**9 + 7
+
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+
+        lcm = (a * b) // gcd(a, b)
+
+        left = min(a, b)
+        right = n * min(a, b)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            count = mid // a + mid // b - mid // lcm
+
+            if count < n:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left % MOD
