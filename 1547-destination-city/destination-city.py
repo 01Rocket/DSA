c6f2b1db3aa37405e58fe364d4cf9f3ecdeb6b9c
Nehal Verma <142144242+01Rocket@users.noreply.github.com>
2026-09-03 09:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.03%) - LeetSync

---
+++ 1547-destination-city/destination-city.py
@@ -0,0 +1,10 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start_cities = set()
+
+        for start, end in paths:
+            start_cities.add(start)
+
+        for start, end in paths:
+            if end not in start_cities:
+                return end
